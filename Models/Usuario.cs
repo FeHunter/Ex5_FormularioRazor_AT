f88c96c10490efd35168ba33440247156794eeb7
FeHunter <hunterfe1111@gmail.com>
2024-09-21 16:12:00 -0300

    Criada classe usuario com validação dos campos e data

---
+++ Models/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ex5_FormularioRazor_AT.Models
+{
+    public class Usuario
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataDeNascimento { get; set; }
+
+        public static ValidationResult ValidarDaDeNascimento(DateTime dataDeNascimento, ValidationContext context)
+        {
+            if (dataDeNascimento > DateTime.Now)
+            {
+                return new ValidationResult("A data de nascimento não pode ser no futuro.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
